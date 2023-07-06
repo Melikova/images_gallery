0d7f735371f7c47e3436991b5dc8a2ea4403539c
Mehin Melikova <mhmelikova@gmail.com>
2023-07-06 16:55:08 +0400

    cards added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import ImageCard from './components/ImageCard/ImageCard';
 
 function App() {
   return (
     <> 
     <Header />
-    <div style={{height:'63vh'}}></div>
+    <Container className="d-flex">
+      <ImageCard />
+      <ImageCard />
+      <ImageCard />
+      <ImageCard />
+      <ImageCard />
+    </Container>
     <Footer />
     </>
   );
