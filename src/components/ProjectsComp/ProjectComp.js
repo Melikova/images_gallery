539fb2b263b17a832f4bb70ae923cbdb6d1d3857
Mehin Melikova <mhmelikova@gmail.com>
2023-07-08 01:07:16 +0400

    slick slider added

--- src/components/ProjectsComp/ProjectComp.js
+++ src/components/ProjectsComp/ProjectComp.js
@@ -31,10 +31,4 @@ export function ProjectsComp(){
         </Slider>
         </Container>
       );
-    // return(
-    //     <Container fluid className='px-2 px-sm-3 px-md-4 px-lg-5 my-5'>
-    //         <div className='h2 text-center mb-4 fw-bolder'>PROJECTS</div>
-    //         <div className='text-center py-5 fs-5' style={{color:'#8D8A88'}}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.</div>
-    //     </Container>
-    // );
 }
